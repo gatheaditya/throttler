59843bf238557c760bbaaaeebe53f5f5d3109358
aditya <gatheaditya@gmail.com>
2020-02-15 19:19:05 +0530

    initial commit

---
+++ src/test/java/com/api/ratelimiter/throttler/ThrottlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ratelimiter.throttler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrottlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
